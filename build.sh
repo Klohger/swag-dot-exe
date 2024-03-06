a891b65f8ff2e619e8b7bd1a724b30040ab8eeb3
KLohger <fanimdude32@gmail.com>
2024-03-06 20:05:51 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-windres swag.rc -O coff -o swag.res
-gcc swag.c -o swag.exe swag.res -mwindows -Oz -s -fdata-sections -ffunction-sections -Wl,--gc-sections -fno-ident -fno-asynchronous-unwind-tables -Wall -Wextra -static
+windres swag.rc -O coff -o x64/swag.res
+gcc swag.c -o x64/swag.exe x64/swag.res -mwindows -Oz -s -fdata-sections -ffunction-sections -Wl,--gc-sections -fno-ident -fno-asynchronous-unwind-tables -Wall -Wextra -static
