0279f5000329bede7f0af0c13bca98762a3824d2
KLohger <fanimdude32@gmail.com>
2024-03-06 20:49:23 +0100

    updated build script and added bat version

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-mkdir -p x64
+@echo off
+if not exist .\x64\NUL mkdir .\x64
 windres swag.rc -O coff -o x64/swag.res
 gcc swag.c -o x64/swag.exe x64/swag.res -mwindows -Oz -s -fdata-sections -ffunction-sections -Wl,--gc-sections -fno-ident -fno-asynchronous-unwind-tables -Wall -Wextra -static
